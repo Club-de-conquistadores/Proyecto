e24cb4fd7027564f27fd36d42089186533c623ed
PC <PC@JUANTORRES>
2024-05-23 18:24:08 -0700

    Agregar archivos de proyecto.

---
+++ Entidades/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Entidades
+{
+    public class Class1
+    {
+        int a;
+
+        public Class1(int a) {
+
+            A = a;
+        
+        }
+
+        public int A { get => a; set => a = value; }
+    }
+}
---
+++ Negocio/ejemplo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema.Entidades;
+
+namespace Sistema.Negocio
+{
+    public class ejemplo
+    {
+        Sistema.Entidades.Class1 entidad;
+
+        public ejemplo(int a) { 
+        
+            Entidad = new Sistema.Entidades.Class1(a);
+
+        }
+
+        public Class1 Entidad { get => entidad; set => entidad = value; }
+    }
+}
