ca6b7b05229b30890b76033e02469e2a18f1b699
JuanTS002 <L20170845@culiacan.tecnm.mx>
2024-05-26 20:24:14 -0700

    matenme

--- Front/Form1.Designer.cs
+++ Front/Form1.Designer.cs
@@ -28,55 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(376, 255);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 89);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(332, 217);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 23);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(356, 83);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 90);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Tengo sueño";
-            this.button2.UseVisualStyleBackColor = true;
+            button1.Image = Properties.Resources.reportee2;
+            button1.Location = new Point(28, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(760, 426);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
-        private TextBox textBox1;
-        private Button button2;
     }
 }
--- Front/Form1.cs
+++ Front/Form1.cs
@@ -11,7 +11,7 @@ using Sistema.Negocio;
 
 namespace Front
 {
-    
+
     public partial class Form1 : Form
     {
 
